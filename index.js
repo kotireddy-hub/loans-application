d1080a75bff089fe7e8a47114db91c3a9d363519
Kotireddy Kamireddy <kotireddyk0485@gmail.com>
2023-02-15 11:00:29 +0530

    get all loans application

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const express = require('express');
+
+//Create an express
 const app = express();
 
+// Handling json bod request
 const bodyParser = require('body-parser');
 app.use(bodyParser,json());
+
+//To include db
+const db = require("./db.js");
+
+app.get("/",function(req,res){
+
+    res.json({
+        status:true,
+        message:"Loans appliaction API is runining successfuly"
+    })
+});
+
+//Get all loans appliactions
+
+app.get("/loans",(req,res)=>{
+
+    db.serialize(()=>{
+        db.all("select * from loans",(error,rows)=>{
+            console.log(rows,"::Rows")
+        })
+    })
+})
